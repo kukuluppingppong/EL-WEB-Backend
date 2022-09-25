5034e600ee8036fce9aeb501159ec249cad4f4fd
unknown <ljjungmin@gmail.com>
2022-09-25 15:02:50 +0900

    initial

---
+++ src/main/java/EL/webBackend/ElWebBackendApplication.java
@@ -0,0 +1,13 @@
+package EL.webBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EL/webBackend/ElWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package EL.webBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
